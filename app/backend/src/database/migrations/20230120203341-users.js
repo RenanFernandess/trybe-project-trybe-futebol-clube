e98ab5b686331a7c84ca4cc75bcb2bb22d9306bc
Renan Fernandes <renzinestuods@gmail.com>
2023-01-20 19:12:50 -0300

    :sparkles: feat: Implementa a migration users

---
+++ app/backend/src/database/migrations/20230120203341-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: Sequelize.STRING,
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
