b8fd884124acdbc10196f9bb83e2c41cfb1bc35f
Renan Fernandes <renzinestuods@gmail.com>
2023-01-20 19:13:31 -0300

    :sparkles: feat: Implementa a migration matches

---
+++ app/backend/src/database/migrations/20230120205528-matches.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('matches', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      homeTeamId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_id',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      homeTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'home_team_goals',
+      },
+      awayTeamId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_id',
+        references: {
+          model: 'teams',
+          key: 'id',
+        },
+      },
+      awayTeamGoals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'away_team_goals',
+      },
+      inProgress: Sequelize.BOOLEAN,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('matches');
+  }
+};
