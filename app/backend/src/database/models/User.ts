226230f6e17e6bec2a87bcf5f76e369a4ad692ed
Renan Fernandes <renzinestuods@gmail.com>
2023-01-24 15:50:43 -0300

    :boom: fix: Declara o username todo em letra minuscula

--- app/backend/src/database/models/User.ts
+++ app/backend/src/database/models/User.ts
@@ -3,7 +3,7 @@ import db from '.';
 
 class User extends Model {
   declare id: number;
-  declare userName: string;
+  declare username: string;
   declare role: string;
   declare email: string;
   declare password: string;
