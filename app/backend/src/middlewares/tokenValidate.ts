ad29184489d04bfc2b2444d3399c3d8b8f5051c1
Renan Fernandes <renzinestuods@gmail.com>
2023-01-26 16:43:51 -0300

    :sparkles: feat: Faz o tratamento de error

--- app/backend/src/middlewares/tokenValidate.ts
+++ app/backend/src/middlewares/tokenValidate.ts
@@ -1,16 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import HttpError from '../errors';
 import Token from '../auth';
 
 const tokenValidate = async (req: Request, res: Response, next: NextFunction) => {
   const { headers: { authorization } } = req;
   const token = new Token();
   try {
-    if (!authorization) throw new Error();
+    if (!authorization) throw new HttpError(401, 'Expired or invalid token');
     const user = await token.Validate(authorization);
     req.body.user = user;
     next();
   } catch (error) {
-    return res.status(401).json({ message: 'Expired or invalid token' });
+    if (error instanceof JsonWebTokenError) {
+      return res.status(401).json({ message: 'Expired or invalid token' });
+    }
+    if (error instanceof HttpError) {
+      const { status, message } = error;
+      return res.status(status).json({ message });
+    }
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
 
