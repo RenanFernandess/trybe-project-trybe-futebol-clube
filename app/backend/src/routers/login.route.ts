98c2ff02e291323bc6760bbf981467e83f981722
Renan Fernandes <renzinestuods@gmail.com>
2023-01-26 17:41:16 -0300

    :sparkles: feat: Cria a rota de login

---
+++ app/backend/src/routers/login.route.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import loginValidate, { tokenValidate } from '../middlewares';
+import LoginService from '../services';
+import LoginController from '../controllers';
+import Token from '../auth';
+
+const token = new Token();
+const loginService = new LoginService(token);
+const loginController = new LoginController(loginService);
+
+const loginRouter = Router();
+
+loginRouter.post('/', loginValidate, (req, res) => { loginController.login(req, res); });
+loginRouter.get('/validate', tokenValidate, (req, res) => { loginController.validate(req, res); });
+
+export default loginRouter;
