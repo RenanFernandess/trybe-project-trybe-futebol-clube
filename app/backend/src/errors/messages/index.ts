1e76954b02c6feb32f00e76ac3b3b59bc8e41a06
Renan Fernandes <renzinestuods@gmail.com>
2023-01-27 19:27:38 -0300

    :sparkles: feat: Armazena as messages de orror em uma constante

---
+++ app/backend/src/errors/messages/index.ts
@@ -0,0 +1,8 @@
+const EQUAL_TEAMS = 'It is not possible to create a match with two equal teams';
+export default EQUAL_TEAMS;
+
+export const TEAM_NOT_FOUND = 'There is no team with such id!';
+
+export const TOKEN_INVALID = 'Token must be a valid token';
+
+export const SERVER_ERROR = 'Internal Server Error';
