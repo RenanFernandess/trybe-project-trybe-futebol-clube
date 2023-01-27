3a9b44c39b2f9ca31cccc53fd4a5818c863d102e
Renan Fernandes <renzinestuods@gmail.com>
2023-01-27 19:18:11 -0300

    :sparkles: feat: Cria a função finish e na função crate faz algumas verificações antes de criar o time

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,6 +1,8 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
-import { TMatches } from '../types';
+import { TMatchesAdd, TMatchesdb, TMatchesTeam } from '../types';
+import HttpError from '../errors';
+import EQUAL_TEAMS, { TEAM_NOT_FOUND } from '../errors/messages';
 
 export default class MatchService {
   public getAll = (inProgress: boolean | undefined): Promise<Match[]> => Match.findAll({
@@ -11,13 +13,35 @@ export default class MatchService {
     where: (typeof inProgress === 'boolean') ? { inProgress } : {},
   });
 
-  public findById = async (id: number | string): Promise<TMatches> => {
-    const { dataValues } = await Match.findByPk(id, {
+  public findById = async (id: number | string): Promise<TMatchesTeam> => {
+    const matches = await Match.findByPk(id, {
       include: [
         { model: Team, as: 'homeTeam', attributes: { exclude: ['id'] } },
         { model: Team, as: 'awayTeam', attributes: { exclude: ['id'] } },
       ],
-    }) || { dataValues: null };
-    return dataValues;
+    });
+    if (!matches) throw new HttpError(404, TEAM_NOT_FOUND);
+    return matches.dataValues;
+  };
+
+  public create = async (
+    { homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals }: TMatchesAdd,
+  ): Promise<TMatchesdb> => {
+    if (homeTeamId === awayTeamId) throw new HttpError(422, EQUAL_TEAMS);
+    await this.findById(homeTeamId);
+    await this.findById(awayTeamId);
+    const match = await Match.create({
+      homeTeamId,
+      homeTeamGoals,
+      awayTeamId,
+      awayTeamGoals,
+      inProgress: true,
+    });
+    return match.dataValues;
+  };
+
+  public finish = async (id: string | number): Promise<string> => {
+    await Match.update({ inProgress: false }, { where: { id } });
+    return 'finished';
   };
 }
