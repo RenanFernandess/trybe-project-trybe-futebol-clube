85f79634dbcf1439707ea31d7d9264cb77533c54
Renan Fernandes <renzinestuods@gmail.com>
2023-01-28 13:43:32 -0300

    :sparkles: feat: Implementa as rotas '/home' e '/away'

--- app/backend/src/routers/leaderboard.router.ts
+++ app/backend/src/routers/leaderboard.router.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import { LeaderboardService } from '../services';
+import { LeaderboardController } from '../controllers';
+
+const leaderboardService = new LeaderboardService();
+const leaderboardController = new LeaderboardController(leaderboardService);
 
 const leaderboardRouter = Router();
 
-leaderboardRouter.get('/');
-leaderboardRouter.get('/home');
-leaderboardRouter.get('/away');
+leaderboardRouter.get('/', leaderboardController.findAll);
+leaderboardRouter.get('/home', leaderboardController.findAll);
+leaderboardRouter.get('/away', leaderboardController.findAll);
 
 export default leaderboardRouter;
