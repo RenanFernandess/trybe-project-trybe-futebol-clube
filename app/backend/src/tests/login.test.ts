8e2f1f5c23a7d529362f97e4cf2400407434c99a
Renan Fernandes <renzinestuods@gmail.com>
2023-01-31 19:17:43 -0300

    :white_check_mark: test: Verifica se é retornado code 401 e a mensagem "Incorrect email or password", caso o email não seja valido

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,38 +7,26 @@ import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
-import { FIELDS_FILLED } from '../errors/messages';
+import { FIELDS_FILLED, INCORRECT_LOGIN } from '../errors/messages';
+// import USER_MOCK from './mocks/user.mock';
+import User from '../database/models/User';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Testa o login', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
-
   // let chaiHttpResponse: Response;
 
-  // before(async () => {
+  // beforeEach(async () => {
   //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
+  //     .stub(User, "findOne")
+  //     .resolves(USER_MOCK as User);
   // });
 
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
+  // afterEach(()=>{
+  //   (User.findOne as sinon.SinonStub).restore();
   // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
 
   it(`Verifica se é retornado code 400 e a mensagem "${FIELDS_FILLED}", caso o email não seja passado.`, async () => {
     const res = await chai
@@ -63,5 +51,21 @@ describe('Testa o login', () => {
 
     expect(res.status).to.be.equal(400);
     expect(res.body).to.be.deep.equal({ message: FIELDS_FILLED })
+  });
+
+  it(`Verifica se é retornado code 401 e a mensagem "${INCORRECT_LOGIN}", caso o email não seja valido.`, async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(null);
+    const res = await chai
+      .request(app)
+      .post('/login')
+      .send({
+        "email": "adminsa@admin.com",
+        "password": "secret_admin"
+      });
+
+    expect(res.status).to.be.equal(401);
+    expect(res.body).to.be.deep.equal({ message: INCORRECT_LOGIN })
   });
 });
