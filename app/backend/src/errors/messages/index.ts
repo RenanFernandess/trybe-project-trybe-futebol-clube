037224c5257985a919d1181c41499ca5f6912395
Renan Fernandes <renzinestuods@gmail.com>
2023-01-31 18:54:01 -0300

    :sparkles: feat: Adiciona a messagem 'Incorrect email or password'

--- app/backend/src/errors/messages/index.ts
+++ app/backend/src/errors/messages/index.ts
@@ -1,10 +1,16 @@
+// 400
+export const FIELDS_FILLED = 'All fields must be filled';
+
+// 401
+export const INCORRECT_LOGIN = 'Incorrect email or password';
+export const TOKEN_INVALID = 'Token must be a valid token';
+
+// 404
+export const TEAM_NOT_FOUND = 'There is no team with such id!';
+
+// 422
 const EQUAL_TEAMS = 'It is not possible to create a match with two equal teams';
 export default EQUAL_TEAMS;
 
-export const TEAM_NOT_FOUND = 'There is no team with such id!';
-
-export const TOKEN_INVALID = 'Token must be a valid token';
-
+// 500
 export const SERVER_ERROR = 'Internal Server Error';
-
-export const FIELDS_FILLED = 'All fields must be filled';
