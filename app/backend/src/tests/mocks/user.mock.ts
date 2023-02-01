60f8603793882ba4e5044d8db031894a30e203e7
Renan Fernandes <renzinestuods@gmail.com>
2023-02-01 19:15:33 -0300

    :test_tube: test: Cria os mocks noPassword, noEmail, invalidEmail, invalidPassword e login

--- app/backend/src/tests/mocks/user.mock.ts
+++ app/backend/src/tests/mocks/user.mock.ts
@@ -1,4 +1,4 @@
-const USER_MOCK = {
+const userMock = {
   dataValues: {
     id: 1,
     username: 'Admin',
@@ -12,5 +12,29 @@ const USER_MOCK = {
   email: 'admin@admin.com',
   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
 }
+export default userMock;
 
-export default USER_MOCK;
+export const noPassword = {
+  "email": "test@gmail.com",
+  "password": ""
+};
+
+export const noEmail = {
+  "email": "",
+  "password": "password"
+};
+
+export const invalidEmail = {
+  "email": "incorrect@admin.com",
+  "password": "secret_admin"
+};
+
+export const invalidPassword = {
+  "email": "admin@admin.com",
+  "password": "incorrect"
+};
+
+export const login = {
+  "email": "admin@admin.com",
+  "password": "secret_admin"
+};
