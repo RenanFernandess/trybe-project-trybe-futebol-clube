b5b2cda6119fa80fbd45dd1de819f88cd21ea1a8
Renan Fernandes <renzinestuods@gmail.com>
2023-02-01 19:45:08 -0300

    :recycle: refactor: Utiliza os mocks do  invalidEmail, invalidPassword, login, noEmail e noPassword deixando o codigo mais limpo

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -7,7 +7,7 @@ import { app } from '../app';
 
 import { Response } from 'superagent';
 import { FIELDS_FILLED, INCORRECT_LOGIN, TOKEN_INVALID } from '../errors/messages';
-import USER_MOCK from './mocks/user.mock';
+import userMock, { invalidEmail, invalidPassword, login, noEmail, noPassword } from './mocks/user.mock';
 import User from '../database/models/User';
 
 chai.use(chaiHttp);
@@ -17,73 +17,37 @@ const { expect } = chai;
 describe('Testa o login', () => {
   describe('Testa a rota POST "/login"', () => {
     it(`Verifica se é retornado code 400 e a mensagem "${FIELDS_FILLED}", caso o email não seja passado.`, async () => {
-      const res = await chai
-        .request(app)
-        .post('/login')
-        .send({
-          "email": "",
-          "password": "password"
-        });
+      const res = await chai.request(app).post('/login').send(noEmail);
   
       expect(res.status).to.be.equal(400);
       expect(res.body).to.be.deep.equal({ message: FIELDS_FILLED })
     });
     it(`Verifica se é retornado code 400 e a mensagem "${FIELDS_FILLED}", caso o password não seja passado.`, async () => {
-      const res = await chai
-        .request(app)
-        .post('/login')
-        .send({
-          "email": "test@gmail.com",
-          "password": ""
-        });
-  
+      const res = await chai.request(app).post('/login').send(noPassword);
+
       expect(res.status).to.be.equal(400);
       expect(res.body).to.be.deep.equal({ message: FIELDS_FILLED })
     });
   
     it(`Verifica se é retornado code 401 e a mensagem "${INCORRECT_LOGIN}", caso o email não seja valido.`, async () => {
-      sinon
-        .stub(User, "findOne")
-        .resolves(null);
-      const res = await chai
-        .request(app)
-        .post('/login')
-        .send({
-          "email": "incorrect@admin.com",
-          "password": "secret_admin"
-        });
+      sinon.stub(User, "findOne").resolves(null);
+      const res = await chai.request(app).post('/login').send(invalidEmail);
   
       expect(res.status).to.be.equal(401);
       expect(res.body).to.be.deep.equal({ message: INCORRECT_LOGIN })
     });
   
     it(`Verifica se é retornado code 401 e a mensagem "${INCORRECT_LOGIN}", caso o password não seja valido.`, async () => {
-      sinon
-        .stub(User, "findOne")
-        .resolves(USER_MOCK as User);
-      const res = await chai
-        .request(app)
-        .post('/login')
-        .send({
-          "email": "admin@admin.com",
-          "password": "incorrect"
-        });
+      sinon.stub(User, "findOne").resolves(userMock as User);
+      const res = await chai.request(app).post('/login').send(invalidPassword);
   
       expect(res.status).to.be.equal(401);
       expect(res.body).to.be.deep.equal({ message: INCORRECT_LOGIN })
     });
   
     it(`Verifica se é possivel fazer login com email e senha validos.`, async () => {
-      sinon
-        .stub(User, "findOne")
-        .resolves(USER_MOCK as User);
-      const res = await chai
-        .request(app)
-        .post('/login')
-        .send({
-          "email": "admin@admin.com",
-          "password": "secret_admin"
-        });
+      sinon.stub(User, "findOne").resolves(userMock as User);
+      const res = await chai.request(app).post('/login').send(login);
   
       expect(res.status).to.be.equal(200);
       expect(typeof res.body.token).to.be.equal('string')
@@ -91,15 +55,10 @@ describe('Testa o login', () => {
   });
 
   describe('Testa a rota GET "login/validate"', () => {
-    beforeEach(() => sinon
-    .stub(User, 'findByPk')
-    .resolves(USER_MOCK as User));
+    beforeEach(() => sinon.stub(User, 'findByPk').resolves(userMock as User));
 
     it(`Verifica se é retornado code 401 e a mensagem "${TOKEN_INVALID}", caso o token não seja fornecido.`, async () => {
-      const res = await chai
-        .request(app)
-        .get('/login/validate')
-        .set({ authorization: '' });
+      const res = await chai.request(app).get('/login/validate').set({ authorization: '' });
 
       expect(res.status).to.be.equal(401);
       expect(res.body.message).to.be.equal(TOKEN_INVALID)
@@ -116,18 +75,8 @@ describe('Testa o login', () => {
     });
 
     it(`Verifica se é retornado code 200 e role do usuario caso o token seja valido.`, async () => {
-      const { body: { token } } = await chai
-      .request(app)
-      .post('/login')
-        .send({
-          "email": "admin@admin.com",
-          "password": "secret_admin"
-        });
-
-      const res = await chai
-        .request(app)
-        .get('/login/validate')
-        .set({ authorization: token });
+      const { body: { token } } = await chai.request(app).post('/login').send(login);
+      const res = await chai.request(app).get('/login/validate').set({ authorization: token });
 
       expect(res.status).to.be.equal(200);
       expect(res.body.role).to.be.equal('admin');
