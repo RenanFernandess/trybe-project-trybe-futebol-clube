7db9a38d5a472e6c258ad588fc183869b7d05345
Renan Fernandes <renzinestuods@gmail.com>
2023-02-02 18:56:19 -0300

    :recycle: refactor: Remove o try catch pois o express-async-errors ja faz coloca implicitamente o try catch

--- app/backend/src/middlewares/tokenValidate.ts
+++ app/backend/src/middlewares/tokenValidate.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import { JsonWebTokenError } from 'jsonwebtoken';
 import HttpError from '../errors';
 import Token from '../auth';
 import { TOKEN_INVALID } from '../errors/messages';
@@ -7,21 +6,10 @@ import { TOKEN_INVALID } from '../errors/messages';
 const tokenValidate = async (req: Request, res: Response, next: NextFunction) => {
   const { headers: { authorization } } = req;
   const token = new Token();
-  try {
-    if (!authorization) throw new HttpError(401, TOKEN_INVALID);
-    const user = await token.Validate(authorization);
-    req.body.user = user;
-    next();
-  } catch (error) {
-    if (error instanceof JsonWebTokenError) {
-      return res.status(401).json({ message: TOKEN_INVALID });
-    }
-    if (error instanceof HttpError) {
-      const { status, message } = error;
-      return res.status(status).json({ message });
-    }
-    return res.status(500).json({ message: 'Internal Server Error' });
-  }
+  if (!authorization) throw new HttpError(401, TOKEN_INVALID);
+  const user = await token.Validate(authorization);
+  req.body.user = user;
+  next();
 };
 
 export default tokenValidate;
