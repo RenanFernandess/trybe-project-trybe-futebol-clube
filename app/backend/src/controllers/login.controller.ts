a3e08a70fec45029b233cb574d76de07cb41d008
Renan Fernandes <renzinestuods@gmail.com>
2023-02-02 18:44:21 -0300

    :boom: fix: Muda o metodo login para arrow function

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -4,10 +4,10 @@ import LoginService from '../services';
 export default class LoginController {
   constructor(private _loginService: LoginService) {}
 
-  public async login({ body: { email, password } }: Request, res: Response) {
+  public login = async ({ body: { email, password } }: Request, res: Response) => {
     const token = await this._loginService.login({ email, password });
     return res.status(200).json({ token });
-  }
+  };
 
   public validate = async (req: Request, res: Response) => {
     const { body: { user: { role } } } = req;
