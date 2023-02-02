4f44b5666867b02f22881c8b076d14560cb59b80
Renan Fernandes <renzinestuods@gmail.com>
2023-02-02 18:23:12 -0300

    :recycle: refactor: Remove o try catch pois express-async-errors capitura o error

--- app/backend/src/controllers/matches.controllor.ts
+++ app/backend/src/controllers/matches.controllor.ts
@@ -1,42 +1,24 @@
 import { Request, Response } from 'express';
-import { SERVER_ERROR } from '../errors/messages';
-import HttpError from '../errors';
 import { MatchService } from '../services';
 
 export default class MatchController {
   constructor(private _MatchService: MatchService) {}
 
   public getAll = async ({ query: { inProgress } }: Request, res: Response) => {
-    try {
-      const query = (typeof inProgress === 'string') ? JSON.parse(inProgress) : undefined;
-      console.log(query);
-      const matches = await this._MatchService.getAll(query);
-      return res.status(200).json(matches);
-    } catch (error) {
-      return res.status(500).json({ message: SERVER_ERROR });
-    }
+    const query = (typeof inProgress === 'string') ? JSON.parse(inProgress) : undefined;
+    console.log(query);
+    const matches = await this._MatchService.getAll(query);
+    return res.status(200).json(matches);
   };
 
   public findById = async ({ params: { id } }: Request, res: Response) => {
-    try {
-      const match = await this._MatchService.findById(id);
-      return res.status(200).json(match);
-    } catch (error) {
-      return res.status(500).json({ message: SERVER_ERROR });
-    }
+    const match = await this._MatchService.findById(id);
+    return res.status(200).json(match);
   };
 
   public create = async ({ body }: Request, res: Response) => {
-    try {
-      const match = await this._MatchService.create(body);
-      res.status(201).json(match);
-    } catch (error) {
-      if (error instanceof HttpError) {
-        const { status, message } = error;
-        return res.status(status).json({ message });
-      }
-      return res.status(500).json({ message: SERVER_ERROR });
-    }
+    const match = await this._MatchService.create(body);
+    res.status(201).json(match);
   };
 
   public finish = async ({ params: { id } }: Request, res: Response) => {
--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -5,20 +5,12 @@ export default class TeamController {
   constructor(private _TeamService: TeamService) {}
 
   public getAll = async (_req: Request, res: Response) => {
-    try {
-      const teams = await this._TeamService.getAll();
-      return res.status(200).json(teams);
-    } catch (error) {
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    const teams = await this._TeamService.getAll();
+    return res.status(200).json(teams);
   };
 
   public findById = async ({ params: { id } }: Request, res: Response) => {
-    try {
-      const teams = await this._TeamService.findById(id);
-      return res.status(200).json(teams);
-    } catch (error) {
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
+    const teams = await this._TeamService.findById(id);
+    return res.status(200).json(teams);
   };
 }
