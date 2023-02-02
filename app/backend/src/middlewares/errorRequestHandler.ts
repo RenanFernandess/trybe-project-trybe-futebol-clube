1b61ba730d8ba0d826885219b5b0634e67883a06
Renan Fernandes <renzinestuods@gmail.com>
2023-02-02 11:41:41 -0300

    :sparkles: feat: Cria o middleware para tratar os errors

---
+++ app/backend/src/middlewares/errorRequestHandler.ts
@@ -0,0 +1,17 @@
+import { ErrorRequestHandler } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
+import HttpError from '../errors';
+import { TOKEN_INVALID } from '../errors/messages';
+
+const errorRequestHandler: ErrorRequestHandler = (error, _req, res, _next) => {
+  if (error instanceof JsonWebTokenError) {
+    return res.status(401).json({ message: TOKEN_INVALID });
+  }
+  if (error instanceof HttpError) {
+    const { status, message } = error;
+    return res.status(status).json({ message });
+  }
+  return res.status(500).json({ message: 'Internal Server Error' });
+};
+
+export default errorRequestHandler;
--- app/backend/src/middlewares/index.ts
+++ app/backend/src/middlewares/index.ts
@@ -1,5 +1,6 @@
 import loginValidate from './loginValidate';
 import tokenValidate from './tokenValidate';
+import errorRequestHandler from './errorRequestHandler';
 
 export default loginValidate;
-export { tokenValidate };
+export { tokenValidate, errorRequestHandler };
