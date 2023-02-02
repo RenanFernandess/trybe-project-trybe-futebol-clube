4a807793e980be51844a98fd0cd93d5140f670eb
Renan Fernandes <renzinestuods@gmail.com>
2023-02-02 18:10:59 -0300

    :sparkles: feat: Utiliza o express-async-errors

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -12,7 +12,7 @@ class App {
     this.config();
 
     // Não remover essa rota
-    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.get('/', (_req, res) => res.json({ ok: true }));
     this.app.use('/login', loginRouter);
     this.app.use('/teams', teamsRouter);
     this.app.use('/matches', matchesRouter);
