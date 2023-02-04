8151c2d0ed467fd4c910fdda4448d3cbe198f741
Renan Fernandes <renzinestuods@gmail.com>
2023-02-04 13:02:37 -0300

    :bug: fix: Corrige o bug que etava verificando se um match existia em vez do time na função create

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -2,7 +2,7 @@ import Team from '../database/models/Team';
 import Match from '../database/models/Match';
 import { TMatchesAdd, TMatchesdb, TMatchesTeam, TScore } from '../types';
 import HttpError from '../errors';
-import EQUAL_TEAMS, { TEAM_NOT_FOUND } from '../errors/messages';
+import EQUAL_TEAMS, { MATCH_NOT_FOUND, TEAM_NOT_FOUND } from '../errors/messages';
 
 export default class MatchService {
   public getAll = (inProgress: boolean | undefined): Promise<Match[]> => Match.findAll({
@@ -20,7 +20,7 @@ export default class MatchService {
         { model: Team, as: 'awayTeam', attributes: ['teamName'] },
       ],
     });
-    if (!matches) throw new HttpError(404, TEAM_NOT_FOUND);
+    if (!matches) throw new HttpError(404, MATCH_NOT_FOUND);
     return matches.dataValues;
   };
 
@@ -28,8 +28,8 @@ export default class MatchService {
     { homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals }: TMatchesAdd,
   ): Promise<TMatchesdb> => {
     if (homeTeamId === awayTeamId) throw new HttpError(422, EQUAL_TEAMS);
-    await this.findById(homeTeamId);
-    await this.findById(awayTeamId);
+    await this._checkTeamExists(homeTeamId);
+    await this._checkTeamExists(awayTeamId);
     const match = await Match.create({
       homeTeamId,
       homeTeamGoals,
@@ -38,6 +38,10 @@ export default class MatchService {
       inProgress: true,
     });
     return match.dataValues;
+  };
+
+  private _checkTeamExists = async (id: number | string): Promise<void> => {
+    if (!await Team.findByPk(id)) throw new HttpError(404, TEAM_NOT_FOUND);
   };
 
   public finish = async (id: string | number): Promise<string> => {
