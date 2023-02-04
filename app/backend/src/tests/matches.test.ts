412c0ef5fadd464c863b3656098a1287f295473f
Renan Fernandes <renzinestuods@gmail.com>
2023-02-04 13:14:30 -0300

    :white_check_mark: test: Testa a rota GET "/:id", verifica se a partida não for encontrada retorna o statusCode 404 e a message "There is no match with such id!"

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -8,14 +8,18 @@ import { app } from '../app';
 import { Response } from 'superagent';
 import Match from '../database/models/Match';
 import matchesMock, { matchMock } from './mocks/matches.mock';
+import { MATCH_NOT_FOUND } from '../errors/messages';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Testa a rota "/matches"', () => {
+
+  afterEach(sinon.restore);
+
   describe('Testa a rota GET "/matches"', () => {
-    it('Verifica se as matches são retornadas', async () => {
+    it('Verifica se as partidas são retornadas.', async () => {
       sinon.stub(Match, 'findAll').resolves(matchesMock as unknown as Match[]);
       const res: Response = await chai.request(app).get('/matches').send();
 
@@ -26,7 +30,7 @@ describe('Testa a rota "/matches"', () => {
     });
   });
   describe('Testa a rota GET "/:id"', () => {
-    it('Verifica se a matches é retornadas', async () => {
+    it('Verifica se a partida é retornadas.', async () => {
       sinon.stub(Match, 'findByPk').resolves(matchMock as unknown as Match);
       const res: Response = await chai.request(app).get('/matches/:id').send();
 
@@ -36,6 +40,14 @@ describe('Testa a rota "/matches"', () => {
       expect(res.body.homeTeam).not.to.have.property('id');
       expect(res.body.awayTeam).to.have.property('teamName');
       expect(res.body.awayTeam).not.to.have.property('id');
+    });
+
+    it(`Verifica se a partida não for encontrada retorna o statusCode 404 e a message "${MATCH_NOT_FOUND}".`, async () => {
+      sinon.stub(Match, 'findByPk').resolves(null);
+      const res: Response = await chai.request(app).get('/matches/:id').send();
+
+      expect(res.status).to.be.equal(404);
+      expect(res.body.message).to.be.equal(MATCH_NOT_FOUND);
     });
   });
   describe('Testa a rota POST "/"', () => {});
