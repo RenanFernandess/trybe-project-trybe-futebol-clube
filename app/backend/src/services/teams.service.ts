4185b946418549971eb048f50b04fd6ac22bf86e
Renan Fernandes <renzinestuods@gmail.com>
2023-02-03 21:14:09 -0300

    :boom: fix: Retorna o dataValues

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -4,5 +4,8 @@ import Team from '../database/models/Team';
 export default class TeamService {
   public getAll = (): Promise<TTeams[]> => Team.findAll();
 
-  public findById = (id: number | string): Promise<TTeams | null> => Team.findByPk(id);
+  public findById = async (id: number | string): Promise<TTeams | null> => {
+    const team = await Team.findByPk(id);
+    return team?.dataValues;
+  };
 }
