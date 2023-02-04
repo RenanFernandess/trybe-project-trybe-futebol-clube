c4a48b4fc7eb005966eba7db4c45192b3d3c7fcb
Renan Fernandes <renzinestuods@gmail.com>
2023-02-04 13:00:35 -0300

    :sparkles: feat: Veridica se o time existe, caso não exista retorna um error

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,11 +1,14 @@
 import { TTeams } from '../types';
 import Team from '../database/models/Team';
+import HttpError from '../errors';
+import { TEAM_NOT_FOUND } from '../errors/messages';
 
 export default class TeamService {
   public getAll = (): Promise<TTeams[]> => Team.findAll();
 
   public findById = async (id: number | string): Promise<TTeams | null> => {
     const team = await Team.findByPk(id);
-    return team?.dataValues;
+    if (!team) throw new HttpError(404, TEAM_NOT_FOUND);
+    return team.dataValues;
   };
 }
