665c688bb7bf57b6bbb8fe01a83e608d36170c05
Renan Fernandes <renzinestuods@gmail.com>
2023-02-07 11:43:31 -0300

    :white_check_marck: test: Testa a rota GET "leaderboard/away", verifica se retorna o status 200 e a tabela de lideres em ordem decrescente

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -8,7 +8,7 @@ import { app } from '../app';
 import { Response } from 'superagent';
 import Team from '../database/models/Team';
 import { teamsWithMatches } from './mocks/teams.mock';
-import leaderboardHomeMock from './mocks/leaderboard.mock';
+import leaderboardHomeMock, { leaderboardAwayMock } from './mocks/leaderboard.mock';
 
 chai.use(chaiHttp);
 const { expect } = chai;
@@ -40,8 +40,21 @@ describe('Testa o Liaderboard', () => {
   
       expect(res.status).to.be.equal(200);
       expect(res.body[0]).to.have.all.keys(keys);
+      expect(res.body[0].name).to.be.match(/Santos/i);
+      expect(res.body[0]).to.be.deep.equal(leaderboardHomeMock[0]);
       expect(res.body).to.be.deep.equal(leaderboardHomeMock);
     });
   });
-
+  
+  describe('Testa a rota GET "leaderboard/away"', () => {
+    it('Verifica se retorna o status 200 e a tabela de lideres em ordem decrescente.', async () => {
+      const res: Response = await chai.request(app).get('/leaderboard/away').send();
+  
+      expect(res.status).to.be.equal(200);
+      expect(res.body[0]).to.have.all.keys(keys);
+      expect(res.body[0].name).to.be.match(/Palmeiras/i);
+      expect(res.body[0]).to.be.deep.equal(leaderboardAwayMock[0]);
+      expect(res.body).to.be.deep.equal(leaderboardAwayMock);
+    });
+  });
 });
