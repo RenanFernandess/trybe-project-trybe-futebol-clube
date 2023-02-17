05c788f2b6e6b3cf383f4f75c0d88351e3e27a6d
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 20:18:41 -0300

    :boom: fix: Passa a utilizar a interface ILogin no lugar do tipo

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,13 +1,13 @@
 import * as bcript from 'bcryptjs';
-import TLogin from '../types';
 import User from '../database/models/User';
 import Token from '../auth';
 import HttpError from '../errors';
+import { ILogin } from '../interfaces';
 
 export default class LoginService {
   constructor(private _Token: Token) {}
 
-  public async login({ email, password }: TLogin): Promise<string> {
+  public async login({ email, password }: ILogin): Promise<string> {
     const user = await User.findOne({ where: { email } });
     if (!user || !(await bcript.compare(password, user.password))) {
       throw new HttpError(401, 'Incorrect email or password');
