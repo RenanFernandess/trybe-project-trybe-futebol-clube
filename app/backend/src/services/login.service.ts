9c1fbc05cd29fb2ee3199f1a48f4d71c665c86d6
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 20:51:25 -0300

    :boom: fix: passa a utilizar a UserModel em vez deutilizar a Model User do sequelize

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -1,14 +1,14 @@
 import * as bcript from 'bcryptjs';
-import User from '../database/models/User';
+import UserModel from '../models';
 import Token from '../auth';
 import HttpError from '../errors';
 import { ILogin } from '../interfaces';
 
 export default class LoginService {
-  constructor(private _Token: Token) {}
+  constructor(private _model: UserModel, private _Token: Token) {}
 
   public async login({ email, password }: ILogin): Promise<string> {
-    const user = await User.findOne({ where: { email } });
+    const user = await this._model.findOne({ email });
     if (!user || !(await bcript.compare(password, user.password))) {
       throw new HttpError(401, 'Incorrect email or password');
     }
