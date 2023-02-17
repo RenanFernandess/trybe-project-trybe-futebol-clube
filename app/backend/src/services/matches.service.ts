76ec7492f899bd2169671988f8ea3f265e8ac763
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 20:20:00 -0300

    :boom: fix: Passa a utilizar interface IMatchTeams, IMatch, IScore no lugar de tipo

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,14 +1,18 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
-import { TMatchesAdd, TMatchesdb, TMatchesTeam, TScore } from '../types';
 import HttpError from '../errors';
 import EQUAL_TEAMS, {
   MATCH_NOT_FOUND,
   NOT_UPDATED,
   TEAM_NOT_FOUND,
 } from '../errors/messages';
+import { IMatchTeams, IMatch, IScore } from '../interfaces';
 
 export default class MatchService {
+  private _checkTeamExists = async (id: number | string): Promise<void> => {
+    if (!await Team.findByPk(id)) throw new HttpError(404, TEAM_NOT_FOUND);
+  };
+
   public getAll = (inProgress: boolean | undefined): Promise<Match[]> => Match.findAll({
     include: [
       { model: Team, as: 'homeTeam', attributes: ['teamName'] },
@@ -17,7 +21,7 @@ export default class MatchService {
     where: (typeof inProgress === 'boolean') ? { inProgress } : {},
   });
 
-  public findById = async (id: number | string): Promise<TMatchesTeam> => {
+  public findById = async (id: number | string): Promise<IMatchTeams> => {
     const matches = await Match.findByPk(id, {
       include: [
         { model: Team, as: 'homeTeam', attributes: ['teamName'] },
@@ -29,8 +33,13 @@ export default class MatchService {
   };
 
   public create = async (
-    { homeTeamId, homeTeamGoals, awayTeamId, awayTeamGoals }: TMatchesAdd,
-  ): Promise<TMatchesdb> => {
+    {
+      homeTeamId,
+      homeTeamGoals,
+      awayTeamId,
+      awayTeamGoals,
+    }: Omit<IMatchTeams, 'id' | 'inProgress'>,
+  ): Promise<IMatch> => {
     if (homeTeamId === awayTeamId) throw new HttpError(422, EQUAL_TEAMS);
     await this._checkTeamExists(homeTeamId);
     await this._checkTeamExists(awayTeamId);
@@ -44,17 +53,13 @@ export default class MatchService {
     return match.dataValues;
   };
 
-  private _checkTeamExists = async (id: number | string): Promise<void> => {
-    if (!await Team.findByPk(id)) throw new HttpError(404, TEAM_NOT_FOUND);
-  };
-
   public finish = async (id: string | number): Promise<string> => {
     const [result] = await Match.update({ inProgress: false }, { where: { id } });
     if (!result) throw new HttpError(404, NOT_UPDATED);
     return 'finished';
   };
 
-  public updateScore = async (id: string, { homeTeamGoals, awayTeamGoals }: TScore) => {
+  public updateScore = async (id: string, { homeTeamGoals, awayTeamGoals }: IScore) => {
     const [result] = await Match.update({
       homeTeamGoals,
       awayTeamGoals,
