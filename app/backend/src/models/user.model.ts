d751611ac8d8699e08a109b0178f2e72e5df2403
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 20:49:56 -0300

    :sparkles: feat: Implementa a função findById

--- app/backend/src/models/user.model.ts
+++ app/backend/src/models/user.model.ts
@@ -3,4 +3,6 @@ import User from '../database/models/User';
 
 export default class UserModel {
   public findOne = (where: Partial<IUser>) => User.findOne({ where });
+
+  public findById = (id: string) => User.findByPk(id);
 }
