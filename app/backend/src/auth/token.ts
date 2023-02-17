41fb0b46b2f518c5aba5e64af8cb247fd160f514
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 20:51:45 -0300

    :boom: fix: passa a utilizar a UserModel em vez deutilizar a Model User do sequelize

--- app/backend/src/auth/token.ts
+++ app/backend/src/auth/token.ts
@@ -1,15 +1,17 @@
 import * as jwt from 'jsonwebtoken';
 import IToken, { IUser } from '../interfaces';
 import 'dotenv/config';
-import User from '../database/models/User';
 import HttpError from '../errors';
 import { TOKEN_INVALID } from '../errors/messages';
+import UserModel from '../models';
 
 export default class Token implements IToken {
   private _secret: jwt.Secret;
   private _options: jwt.SignOptions;
+  private _model: UserModel;
 
-  constructor() {
+  constructor(model: UserModel) {
+    this._model = model;
     this._secret = process.env.JWT_SECRET || '';
     this._options = {
       algorithm: 'HS256',
@@ -24,7 +26,7 @@ export default class Token implements IToken {
   public async Validate(token: string): Promise<IUser> {
     const decoded = jwt.verify(token, this._secret) as jwt.JwtPayload;
     const { id } = decoded;
-    const user = await User.findByPk(id);
+    const user = await this._model.findById(id);
     if (!user) {
       throw new HttpError(401, TOKEN_INVALID);
     }
