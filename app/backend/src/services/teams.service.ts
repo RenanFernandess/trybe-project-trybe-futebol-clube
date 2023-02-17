18a979794af47125caf4d7530303c3c6b13c0e46
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 20:21:59 -0300

    :boom: fix: Passa a utilizar a interface ITeam no lugar de tipo

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,12 +1,12 @@
-import { TTeams } from '../types';
 import Team from '../database/models/Team';
 import HttpError from '../errors';
 import { TEAM_NOT_FOUND } from '../errors/messages';
+import { ITeam } from '../interfaces';
 
 export default class TeamService {
-  public getAll = (): Promise<TTeams[]> => Team.findAll();
+  public getAll = (): Promise<ITeam[]> => Team.findAll();
 
-  public findById = async (id: number | string): Promise<TTeams | null> => {
+  public findById = async (id: number | string): Promise<ITeam | null> => {
     const team = await Team.findByPk(id);
     if (!team) throw new HttpError(404, TEAM_NOT_FOUND);
     return team.dataValues;
