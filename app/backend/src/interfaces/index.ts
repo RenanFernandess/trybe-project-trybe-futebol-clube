6f71bd46dbb87bac6c9aa8e9e4f855dfba59473d
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 18:56:56 -0300

    :sparkles: feat: exporta todas as interfaces

--- app/backend/src/interfaces/index.ts
+++ app/backend/src/interfaces/index.ts
@@ -1,3 +1,20 @@
 import IToken from './token';
+import ILogin from './login';
+import IUser from './user';
+import ITeam, { ITeamMatches } from './team';
+import IMatch, { IMatchTeams } from './match';
+import ILeaderboard from './leaderboard';
+import IScore, { IScoreWithId } from './score';
 
 export default IToken;
+export {
+  ILogin,
+  IUser,
+  ITeam,
+  IMatch,
+  ILeaderboard,
+  ITeamMatches,
+  IMatchTeams,
+  IScore,
+  IScoreWithId,
+};
