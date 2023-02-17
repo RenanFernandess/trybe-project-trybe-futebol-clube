3f3fe927d78b7186f402a7c13e7d8363b3144765
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 18:37:06 -0300

    :sparkles: feat: Cria a interface ILeaderboard

---
+++ app/backend/src/interfaces/leaderboard.ts
@@ -0,0 +1,12 @@
+export default interface ILeaderboard {
+  name: string,
+  totalPoints: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsBalance: number,
+  totalGames: number,
+  efficiency: string,
+}
