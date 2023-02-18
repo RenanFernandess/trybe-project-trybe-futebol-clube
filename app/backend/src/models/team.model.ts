7938a20907847628ebdf67cb16f2dd28943a7a6d
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 21:16:40 -0300

    :saparkles: feat: Implementa as funções anonimas findAll e findById

--- app/backend/src/models/team.model.ts
+++ app/backend/src/models/team.model.ts
@@ -1,7 +1,12 @@
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
+import { ITeam } from '../interfaces';
 
 export default class TeamModel {
+  public findAll = (where: Partial<ITeam>) => Team.findAll({ where });
+
+  public findById = (id: string) => Team.findByPk(id);
+
   public findTeamWhithMatches = () => {
     const options = { model: Match, where: { inProgress: false }, as: 'homeTeam' };
     return Team.findAll({
