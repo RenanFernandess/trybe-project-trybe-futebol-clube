63480754e30fe39de643e34dd2df599ea3e51e78
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 21:54:06 -0300

    :sparkles: feat: Implementa a função anonima uddate

--- app/backend/src/models/match.model.ts
+++ app/backend/src/models/match.model.ts
@@ -3,7 +3,12 @@ import Match from '../database/models/Match';
 import { IMatch } from '../interfaces';
 
 export default class MatchModel {
-  public create = async (match: Omit<IMatch, 'id'>) => Match.create(match);
+  public create = (match: Omit<IMatch, 'id'>) => Match.create(match);
+
+  public update = (
+    fields: Partial<IMatch>,
+    where: Partial<IMatch>,
+  ) => Match.update(fields, { where });
 
   public findAll = (where: Partial<IMatch> = {}) => Match.findAll({
     include: [
