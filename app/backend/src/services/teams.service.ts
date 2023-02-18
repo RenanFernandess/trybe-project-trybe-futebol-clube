c57a78e31c62e5fd5c07a61e56ac362cfdfb225f
Renan Fernandes <renzinestuods@gmail.com>
2023-02-17 21:21:45 -0300

    :boom: fix: Agora utiliza a model TeamModel em no lugar da Model Team do sequelize

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,13 +1,15 @@
-import Team from '../database/models/Team';
+import { TeamModel } from '../models';
 import HttpError from '../errors';
 import { TEAM_NOT_FOUND } from '../errors/messages';
 import { ITeam } from '../interfaces';
 
 export default class TeamService {
-  public getAll = (): Promise<ITeam[]> => Team.findAll();
+  constructor(private _model: TeamModel) {}
 
-  public findById = async (id: number | string): Promise<ITeam | null> => {
-    const team = await Team.findByPk(id);
+  public getAll = (): Promise<ITeam[]> => this._model.findAll();
+
+  public findById = async (id: string): Promise<ITeam | null> => {
+    const team = await this._model.findById(id);
     if (!team) throw new HttpError(404, TEAM_NOT_FOUND);
     return team.dataValues;
   };
